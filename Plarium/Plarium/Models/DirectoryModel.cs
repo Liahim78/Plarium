35a653a250b336f17ecaedf445b0f70a6157c128
Misha <Misha@MISHA->
2016-06-06 10:04:59 +0300

    Create DirectoryTree

--- Plarium/Plarium/Models/DirectoryModel.cs
+++ Plarium/Plarium/Models/DirectoryModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,25 @@ namespace Plarium.Models
 {
     class DirectoryModel
     {
-
+        DirectoryTree myTree;
+        public void GetInfoDirectory(string directory, List<string> listSubDirectories, List<string> listFiles, string infoDirectory)
+        {
+            DirectoryInfo myDir = new DirectoryInfo(@directory);
+            foreach (var item in myDir.GetDirectories())
+            {
+                listSubDirectories.Add(item.Name);
+            }
+            foreach (var item in myDir.GetFiles())
+            {
+                listFiles.Add(item.Name);
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.Append("Название - ").Append(myDir.Name).Append("\n");
+            builder.Append("Дата создания - ").Append(myDir.CreationTime).Append("\n");
+            builder.Append("Дата модификации - ").Append(myDir.LastWriteTime).Append("\n");
+            builder.Append("Дата последнего доступа - ").Append(myDir.LastAccessTime).Append("\n");
+            builder.Append("Атрибуты - ").Append(myDir.Attributes).Append("\n");
+            myTree = new DirectoryTree(myDir);
+        }
     }
 }
---
+++ Plarium/Plarium/Models/DirectoryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plarium.Models
+{
+    class DirectoryTree
+    {
+        DirectoryInfo directoryValue;
+        List<DirectoryTree> listSubDirectories;
+        FileInfo [] listFiles;
+
+        public DirectoryTree(DirectoryInfo directoryValue)
+        {
+            this.directoryValue = directoryValue;
+            foreach (var item in directoryValue.GetDirectories())
+            {
+                listSubDirectories.Add(new DirectoryTree(item));
+            }
+            listFiles = directoryValue.GetFiles();
+        }
+    }
+}
