9bb1ff35e6e30554f923c2312e26b86ab330e2c3
Misha <Misha@MISHA->
2016-06-08 17:02:18 +0300

    Last redaction

--- Plarium/Plarium/Views/HelpWindow.xaml.cs
+++ Plarium/Plarium/Views/HelpWindow.xaml.cs
@@ -25,26 +25,26 @@ namespace Plarium.Views
             StringBuilder builder = new StringBuilder();
             builder.Append("  Данная программа позволяет\n");
             builder.Append("пользователю создавать xml\n");
-            builder.Append("фаил, в который можно записать\n");
+            builder.Append("файл, в который можно записать\n");
             builder.Append("информацию о выбранной папке.\n");
             builder.Append("  При запуске программы\n");
             builder.Append("нажмите на кнопку \"Выбрать\"\n");
             builder.Append("и введите полный путь к папке.\n");
             builder.Append("В результате в окне\n");
-            builder.Append("появятся два списка, текс\n");
+            builder.Append("появятся два списка, текст\n");
             builder.Append("и две кнопки. В первом списке\n");
             builder.Append("хранятся названия всех\n");
             builder.Append("поддиректорий, во втором - \n");
             builder.Append("все файлы из этой папки.\n");
             builder.Append("В тексте записана информация\n");
-            builder.Append("о дирректории. И две кнопки\n");
+            builder.Append("о директории. И две кнопки\n");
             builder.Append("для записи xml файла.\n");
             builder.Append("Одна кнопка всегда записывает\n");
             builder.Append("информацию о главной папке.\n");
             builder.Append("А так как можно переходить\n");
             builder.Append("по поддиректориям, то вторая\n");
             builder.Append("кнопка записывает информацию\n");
-            builder.Append("о текущей дирректории.\n");
+            builder.Append("о текущей директории.\n");
             TextHelp.Text = builder.ToString();
         }
     }
